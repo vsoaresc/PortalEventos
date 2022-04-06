b7ce3343d68bf6a890473f622c1f851ea49c0cbb
vitor <vsoarescsilva@gmail.com>
2022-04-06 15:30:08 -0300

    Add project files.

---
+++ ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "1ª Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Tema = "Angular 11",
+                Local = "Rio de Janeiro",
+                Lote = "2ª Lote",
+                QtdPessoas = 100,
+                DataEvento = DateTime.Now.AddDays(1).ToString(),
+                ImagemURL = "foto.png"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+    }
+}
